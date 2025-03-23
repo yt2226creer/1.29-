0fd09fb9d64acca642c61140e329071193842c55
yt2226creer <tuji6222home@gmail.com>
2025-03-23 23:00:27 +0900

    0000000

--- 文系大学生のためのJS入門/NO2/script.js
+++ 文系大学生のためのJS入門/NO2/script.js
@@ -9,3 +9,7 @@ height  = 175;
 
 console.log('体重' + weight + 'kg／身長' + height + 'cmのBMI：');
 console.log(weight / (1.55 * 1.55)); // → 20.20..
+
+let x = 10; // 正しい変数宣言
+y = 20; // letなしでもエラーにはならない
+console.log('x:' + x + ' y:' + y); // → x:10 y:20
