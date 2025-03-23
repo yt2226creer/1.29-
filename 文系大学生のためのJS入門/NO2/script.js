5e63e1e9a61c790267334ca92ffc236362f12d16
yt2226creer <tuji6222home@gmail.com>
2025-03-23 19:16:49 +0900

    11111

--- 文系大学生のためのJS入門/NO2/script.js
+++ 文系大学生のためのJS入門/NO2/script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!!!');
+let weight = 55;
+let height = 155; 
+
+console.log('体重' + weight + 'kg／身長' + height + 'cmのBMI：');
+console.log(weight / (1.55 * 1.55)); // → 20.20..
+
+weight  = 65;
+height  = 175;
+
+console.log('体重' + weight + 'kg／身長' + height + 'cmのBMI：');
+console.log(weight / (1.55 * 1.55)); // → 20.20..
