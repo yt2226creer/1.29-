f03c3ab9919c1afa9e02e6efea9008d2074c95ff
yt2226creer <tuji6222home@gmail.com>
2025-03-24 00:14:46 +0900

    3/24　深夜終了

--- 文系大学生のためのJS入門/NO3/script.js
+++ 文系大学生のためのJS入門/NO3/script.js
@@ -1,4 +1,15 @@
 'use strict';
 
-const bmi = 27;
-console.log(`あなたのBMIは${bmi}です`);
+const bmi = window.prompt('27');
+const p_element = document.querySelector('p');
+console.log(`あなたのBMIは${bmi}：`);
+
+if ( bmi >= 25){
+    p_element.textContent += '肥満です';
+}
+ else if (bmi < 18.5){
+    p_element.textContent += '低体重です';
+}
+  else{
+    p_element.textContent += '適正体重です';
+  }
